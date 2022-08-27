1963c868159289746567176e10f1a4b4a93a34a4
richrd77@gmail.com <richrd77@gmail.com>
2022-08-27 23:24:59 +0530

    capacitor added

---
+++ android/app/src/main/java/com/vplayer/ric/MainActivity.java
@@ -0,0 +1,5 @@
+package com.vplayer.ric;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.vplayer.ric',
+  appName: 'v-player',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
